15d853ebda5e5187bfd0bd94cb67de130ba814f7
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2024-03-08 09:01:53 -0800

    Weather App

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('Wind-speed');
+const location_not_found = document.querySelector(`.location-not-found`);
+const weather_body = document.querySelector( `.weather-body`);
+
+ async function checkWeather(city){
+const api_key = "147712eaad23d97f3f88478d13256119";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+const weather_data = await fetch(`${url}`).then(response => response.json());
+
+if(weather_data.cod===`404`){location_not_found.style.display ="flex";
+weather_body.style.display = "none";
+    console.log("Error")
+    return;
+}
+location_not_found.style.display="none";
+weather_body.style.display = "flex";
+
+temperature.innerHTML = `${Math.round(weather_data.main.temp-273.15)* 9/5 + 32}°F`;
+description.innerHTML = `${weather_data.weather[0].description}`;
+humidity.innerHTML = `${weather_data.main.humidity}%`;
+wind_speed.innerHTML = weather_data['wind']['speed'];
+
+switch(weather_data.weather[0].main){
+    case 'Clouds':
+        weather_img.src = "/images/cloud.png";
+        break;
+        case 'Clear':
+        weather_img.src = "/images/clear.png";
+        break;
+        case 'Rain':
+        weather_img.src = "/images/rain.png";
+        break;
+        case 'Mist':
+        weather_img.src = "/images/mist.png";
+        break;
+        case 'Snow':
+        weather_img.src = "/images/snow.png";
+        break;
+}
+}
+searchBtn.addEventListener('click',()=>{
+checkWeather(inputBox.value);
+});
